044e1eab9cedd2e69c01853517ebbf2b053bc0bc
Pablo Galano <pablogalano@gmail.com>
2015-04-06 12:37:38 -0300

    Slugify Refactored

--- plugins/slugify/SlugifyTwigExtension.php
+++ plugins/slugify/SlugifyTwigExtension.php
@@ -20,30 +20,7 @@ class SlugifyTwigExtension extends \Twig_Extension
 
     public function slugify($text)
     {
-        // replace non letter or digits by -
-        $text = preg_replace('#[^\\pL\d]+#u', '-', $text);
-
-        // trim
-        $text = trim($text, '-');
-
-        // transliterate
-        if (function_exists('iconv'))
-        {
-            $text = iconv('utf-8', 'us-ascii//TRANSLIT', $text);
-        }
-
-        // lowercase
-        $text = strtolower($text);
-
-        // remove unwanted characters
-        $text = preg_replace('#[^-\w]+#', '', $text);
-
-        if (empty($text))
-        {
-            return 'n-a';
-        }
-
-        return $text;
+        return \lib\pathFunctions::slugify($text);
     }
     
 }

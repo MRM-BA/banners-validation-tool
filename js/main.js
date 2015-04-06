66388a0663d3a9a668994b0b2ab9f1d95571302f
Sebastian <sebastian_litter@hotmail.com>
2015-04-06 14:20:36 -0300

    copy to clipboard without tabs

--- js/main.js
+++ js/main.js
@@ -1,1 +1,7 @@
-
+ $("#copy-button").zclip({
+	path:'js/vendor/jquery/ZeroClipboard.swf',
+	copy: $.trim($("div#copy ul").text()).replace(/\s{2,}/g, '\n'), 
+	afterCopy: function copiado(){
+					$("p#copied").show("slow");
+				}
+}); 

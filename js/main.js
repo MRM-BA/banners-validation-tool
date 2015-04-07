738f1f820a992f66f7bdc92d5d52f34c7a6bf762
Pablo Galano <pablogalano@gmail.com>
2015-04-07 17:22:45 -0300

    Bug for copied text fixed

--- js/main.js
+++ js/main.js
@@ -1,7 +1,12 @@
- $("#copy-button").zclip({
-	path:'js/vendor/jquery/ZeroClipboard.swf',
-	copy: $.trim($("div#copy ul").text()).replace(/\s{2,}/g, '\n'), 
-	afterCopy: function copiado(){
-					$("p#copied").show("slow");
-				}
-}); 
+ $(document).ready(function(){
+    $("#copy-button").zclip({
+    path:'js/vendor/jquery/ZeroClipboard.swf',
+    //copy: $.trim($("div#copy ul").text()).replace(/\s{2,}/g, '\n'), 
+    copy: $.trim($("div#copy ul").text()).split("  ").join("").split("\n\n").join("\n"), 
+    afterCopy:  function copied(){
+                    $("p#copied").show("slow", function() {
+                        $("html, body").animate({ scrollTop: $(document).height() }, 1000);
+                    });
+                }
+    });
+});

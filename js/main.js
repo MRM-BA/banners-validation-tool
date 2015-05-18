bb81b369464c143b2e880605c6fd2b17e36e01d4
zev000 <spda2008@gmail.com>
2015-05-18 19:06:20 -0300

    Additional Error´s Info

--- js/main.js
+++ js/main.js
@@ -1,12 +1,26 @@
- $(document).ready(function(){
-    $("#copy-button").zclip({
-    path:'js/vendor/jquery/ZeroClipboard.swf',
-    //copy: $.trim($("div#copy ul").text()).replace(/\s{2,}/g, '\n'), 
-    copy: $.trim($("div#copy ul").text()).split("  ").join("").split("\n\n").join("\n"), 
-    afterCopy:  function copied(){
-                    $("p#copied").show("slow", function() {
-                        $("html, body").animate({ scrollTop: $(document).height() }, 1000);
-                    });
-                }
-    });
+$(document).ready(function () { 
+  project = $('h2#projectName').text(); 
+  detail = 'The file "{{unit}}" contains the following errors: \r\n',
+  message = 'PROJECT: ' + project  + '\r\n';
+  message +=  'UNIT: ' + $('#nameBanner').text() + '\r\n';
+  file = '', 
+  errors = $("div#copy ul li").each(function (index) {
+
+    if ($(this).attr('class') !== file) {
+      message += '\r\n' + detail.replace("{{unit}}", $(this).attr('class'));
+    }
+    file = $(this).attr('class');
+    message += '-' + $(this).text() + '\r\n';
+
+  });
+ 
+  $("#copy-button").zclip({
+    path: 'js/vendor/jquery/ZeroClipboard.swf',
+    copy: message,
+    afterCopy: function copied() {
+      $("p#copied").show("slow", function () {
+        $("html, body").animate({scrollTop: $(document).height()}, 1000);
+      });
+    }
+  });
 });
